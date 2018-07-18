22f10557f681aa74667849b01ee8ef00975345b4
EC2 Default User <ec2-user@ip-172-31-19-93.us-east-2.compute.internal>
2018-07-18 19:43:17 +0000

    rock

--- app/Bot/bot.rb
+++ app/Bot/bot.rb
@@ -1,10 +1,13 @@
 
 
 require 'facebook/messenger'
+
 include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+    if message.text.include? "Bonjour"
+       message.reply(text: 'TG !!!')
+    end
 end
