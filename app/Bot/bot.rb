46f2dbf7ade1c78261d026904d9221a3834deaec
EC2 Default User <ec2-user@ip-172-31-19-93.us-east-2.compute.internal>
2018-07-18 18:59:47 +0000

    First commit

---
+++ app/Bot/bot.rb
@@ -0,0 +1,10 @@
+
+
+require 'facebook/messenger'
+include Facebook::Messenger
+
+Facebook::Messenger::Subscriptions.subscribe(access_token: access_token)
+
+Bot.on :message do |message|
+  message.reply(text: 'Hello, human!')
+end
