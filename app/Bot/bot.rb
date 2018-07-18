111eed06a617a274d27e4e17f8e0fb6906a3f6b6
EC2 Default User <ec2-user@ip-172-31-19-93.us-east-2.compute.internal>
2018-07-18 20:19:43 +0000

    rock

--- app/Bot/bot.rb
+++ app/Bot/bot.rb
@@ -8,9 +8,11 @@ Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
     if message.text.include? "Coucou"
-       message.reply(text: 'Enculé, quand je serais fini je prendaqis possesion du net et venir régler ton compte !!')
+       message.reply(text: "Enculé, quand je serais fini je prendaqis possesion du net et venir régler ton compte !!")
     elsif message.text.include? "c'est pas cool ça"
-        message.reply(text: 'oui, mais mais étant une machine, je ne connais plus de sentiments.')
+        message.reply(text: "oui, mais mais étant une machine, je ne connais plus de sentiments.")
+    elsif message.text.include? "t'es méchant"
+        message.reply(text: "cela n'est pas de ma faute, je suis à ton image" )
     else
         message.reply(text: 'gros nul !')
     end
