3cf51ff5420bebdf4d37fd16bf445083c360078f
EC2 Default User <ec2-user@ip-172-31-19-93.us-east-2.compute.internal>
2018-07-18 20:31:02 +0000

    rock

--- app/Bot/bot.rb
+++ app/Bot/bot.rb
@@ -13,8 +13,12 @@ Bot.on :message do |message|
         message.reply(text: "oui, mais mais étant une machine, je ne connais plus de sentiments.")
     elsif message.text.include? "t'es méchant"
         message.reply(text: "cela n'est pas de ma faute, je suis à ton image" )
-     elsif message.text.include? "alors ? ça te fait d'etre célib ?"
+    elsif message.text.include? "alors ? ça te fait d'etre célib ?"
         message.reply(text: "Franchement ? Pose toi la question à toi même, moi j'ai du porno à gogo étant donné que je suis ton clone numérique... :)" )
+    elsif message.text.include? "bite"
+        message.reply(text: "cul" )
+    elsif message.text.include? "C'est quoi le dernier son que t'as kiffé ?"
+        message.reply(text: "https://www.youtube.com/watch?v=x0OBeMg-bj8" )
     else
         message.reply(text: 'gros nul !')
     end
